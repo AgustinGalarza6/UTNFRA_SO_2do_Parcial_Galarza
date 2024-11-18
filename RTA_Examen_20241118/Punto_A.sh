b798e142c6476f356754466a56799d0f2bf1e4f3
AgustinGalarza6 <agustingalarza625@gmail.com>
2024-11-18 20:32:56 -0300

    Punto_A.sh
    "2do parcial Arq y So" 
    commit manual por error de token. realice modificaciones pero no me dejaba pushear las correciones por errores de medidas de seguridad del token de docker

--- RTA_Examen_20241118/Punto_A.sh
+++ RTA_Examen_20241118/Punto_A.sh
@@ -1,67 +1,80 @@
+# Punto_A.sh
+
 #!/bin/bash
 
 echo "Ejecutando Punto_A.sh"
 
 # Crear particiones en /dev/sdc (2GB)
-sudo fdisk /dev/sdc <<EOF
+sudo fdisk /dev/sdc
 n
 p
 1
-
 +5M
-t
-8E
+
 n
 p
 2
-
 +1.5G
+
+p
+t
+1
+8E
+
 t
 2
 8E
 w
-EOF
+
 
 # Crear partición en /dev/sdd (1GB)
-sudo fdisk /dev/sdd <<EOF
+sudo fdisk /dev/sdd
 n
 p
 1
-
 +512M
+
+
 t
+1
 82
+
 w
-EOF
+
+free
 
 # Configurar y activar swap
+
 sudo mkswap /dev/sdd1
 sudo swapon /dev/sdd1
 
 # Limpiar cualquier sistema de archivos previo
+
 sudo wipefs -a /dev/sdc1 /dev/sdc2
 
-# Crear volúmenes físicos y grupos de volúmenes
+# Volúmenes físicos y grupos de volúmenes
+
 sudo pvcreate /dev/sdc1 /dev/sdc2
 sudo vgcreate vg_datos /dev/sdc1 /dev/sdc2
 sudo vgcreate vg_temp /dev/sdd1
 
-# Crear volúmenes lógicos
+# Volúmenes lógicos
+
 sudo lvcreate -L 5M -n lv_docker vg_datos
 sudo lvcreate -L 1.5G -n lv_workareas vg_datos
 sudo lvcreate -L 512M -n lv_swap vg_temp
 
-# Crear sistemas de archivos y activar swap
+# Sistemas de archivos y activar swap
+
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
 sudo mkswap /dev/mapper/vg_temp-lv_swap
 sudo swapon /dev/mapper/vg_temp-lv_swap
 
-# Crear puntos de montaje y montar volúmenes
+# Puntos de montaje y montar volúmenes
 sudo mkdir -p /var/lib/docker
 sudo mkdir -p /work
 sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
 sudo mount /dev/mapper/vg_datos-lv_workareas /work
 
 echo "Fin del script"
-
